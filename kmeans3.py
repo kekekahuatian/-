bd54db4cfea0de1c5574010037336cb9c8c1f9fc
kekeahuatian <2656938986@qq.com>
2020-12-28 12:32:08 +0800

    更新时间输出

--- kmeans3.py
+++ kmeans3.py
@@ -8,12 +8,12 @@ import os
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 
-def distEclud(vecA, vecB):
+def distEclud(dataSet, centroids):
     res = []
-    for i in range(vecB.shape[0]):
+    for i in range(centroids.shape[0]):
         temp = []
-        for j in range(vecA.shape[0]):
-            a = np.sqrt((vecA[j] - vecB[i]) ** 2)
+        for j in range(dataSet.shape[0]):
+            a = np.sqrt((dataSet[j] - centroids[i]) ** 2)
             b = np.sum(a, axis=1)
             temp.append(b)
         res.append(np.array(temp))
@@ -42,16 +42,11 @@ def kMeans(dataSet, k):
     while True:
         start = time.time()
         court += 1
-        # 计算每个点到中心点的距离
-        # 前2560为所有点到第一个中心的距离
         dataSet = np.array(dataSet)
         dist = distEclud(dataSet, centroids)
-
         dist = torch.Tensor(dist)
         label = dist.argmin(0)
-        # 找新中心
         label = label.unsqueeze(2)
-        # data = torch.cat([dataSet, label], dim=2)
         cmp_labels = label.expand(n, m, 3)
         oldCentroids = centroids.copy()
         temp = []
@@ -60,7 +55,6 @@ def kMeans(dataSet, k):
             a = torch.where(cmp_labels == i, dataSet.type(torch.DoubleTensor), 0.0)
             centroidsT = torch.Tensor(centroids)
             temp.append(torch.where(a != 0, centroidsT[i].type(torch.DoubleTensor), 0.0))
-            # a = np.array(a)
             num = 0
             den = 0
             for j in range(n):
@@ -70,8 +64,8 @@ def kMeans(dataSet, k):
             centroids[i][0] = num[0] / den[0]
             centroids[i][1] = num[1] / den[1]
             centroids[i][2] = num[2] / den[2]
-        print(court)
-        print(time.time() - start)
+        print("第{:.0f}轮：{:.2f}".format(court, time.time() - start))
+
         if (oldCentroids == centroids).all():
             break
     res = torch.zeros(n, m, 3)
@@ -80,8 +74,10 @@ def kMeans(dataSet, k):
     return res.int()
 
 
+start = time.time()
 data = mpimg.imread('1.jpeg')
-img = kMeans(data, 20)
+img = kMeans(data, 2)
 
 plt.imshow(img)
 plt.show()
+print("总耗时：{:.2f}".format(-start + time.time()))
